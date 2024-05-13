d04acafd90831233c8de96d6b05cbe40d4f70c51
Jean-Paul Dormoy <jp.dormoy@yahoo.com>
2024-05-13 15:09:51 -0400

    Test 1

--- src/network.py
+++ src/network.py
@@ -21,6 +21,7 @@ Remember to open the Python Shell from within the src folder (where the source f
 >>> net = network.Network([784, 30, 10])
 
 >>> net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+Added for test. DELETE this line
 '''
 
 #### Libraries
